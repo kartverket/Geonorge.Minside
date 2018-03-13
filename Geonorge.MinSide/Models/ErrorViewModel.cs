6660c60fa7eedf92ee942282c140d7542cec46e7
Dag Olav Dahle <dagolav@arkitektum.no>
2018-03-13 11:13:46 +0100

    Add project file

---
+++ Geonorge.MinSide/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Geonorge.MinSide.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
