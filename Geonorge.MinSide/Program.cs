72e9c4264a5c88d774b42b7f11836f248ff50b0d
Henning Jensen <henning@arkitektum.no>
2018-06-18 15:28:16 +0200

    Add logging

--- Geonorge.MinSide/Program.cs
+++ Geonorge.MinSide/Program.cs
@@ -21,6 +21,12 @@ namespace Geonorge.MinSide
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
                 .Build();
     }
 }
