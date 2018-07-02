262dad5f16d70a27108b2562845f37380a8d0e1f
Henning Jensen <henning@arkitektum.no>
2018-07-02 11:02:34 +0200

    Setup infrastructure for SPA

--- Geonorge.MinSide.Web/Startup.cs
+++ Geonorge.MinSide.Web/Startup.cs
@@ -2,10 +2,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
-using Microsoft.WindowsAzure.Storage;
 
 namespace Geonorge.MinSide
 {
@@ -54,6 +54,10 @@ namespace Geonorge.MinSide
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
             }
             else
             {
@@ -73,6 +77,9 @@ namespace Geonorge.MinSide
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
