d9a8e68361b6f30cbbcd2cd1b549e41e76ee1efd
Henning Jensen <henning@arkitektum.no>
2018-07-11 11:04:52 +0200

    Add basic Geonorge layout

--- Geonorge.MinSide.Web/ClientApp/components/app-root.vue
+++ Geonorge.MinSide.Web/ClientApp/components/app-root.vue
@@ -1,16 +1,20 @@
 <template>
+    <div class="container">
+            <router-view></router-view>
+    </div>
+<!--
     <div id="app" class="container-fluid">
         <div class="row">
             <div class="col-sm-3">
                 <nav-menu params="route: route"></nav-menu>
             </div>
             <div class="col-sm-9">
-                <router-view></router-view>
+        
             </div>
         </div>
 
     </div>
-
+-->
 </template>
 
 <script>
--- Geonorge.MinSide.Web/webpack.config.vendor.js
+++ Geonorge.MinSide.Web/webpack.config.vendor.js
@@ -1,7 +1,9 @@
 ﻿const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = (env) => {
     const extractCSS = new ExtractTextPlugin('vendor.css');
@@ -14,11 +16,23 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' },
-                { test: /\.css(\?|$)/, use: extractCSS.extract(['css-loader']) }
+                { test: /\.css(\?|$)/, use: extractCSS.extract(['css-loader']) },
+                { test: /\.scss$/, use: ExtractTextPlugin.extract({ use: 'css-loader?minimize!sass-loader' }) },
             ]
         },
         entry: {
-            vendor: ['bootstrap', 'bootstrap/dist/css/bootstrap.css', 'event-source-polyfill', 'vue', 'vuex', 'axios', 'vue-router', 'jquery'],
+            vendor: [
+                'bootstrap', 
+                'bootstrap/dist/css/bootstrap.css', 
+                'event-source-polyfill', 
+                'vue', 
+                'vuex', 
+                'axios', 
+                'vue-router', 
+                'jquery',
+                'geonorge-base/src/sass/base.scss',
+                'geonorge-shared-partials/src/css/pageTop.css'                
+            ],
         },
         output: {
             path: path.join(__dirname, 'wwwroot', 'dist'),
@@ -27,13 +41,22 @@ module.exports = (env) => {
             library: '[name]_[hash]',
         },
         plugins: [
+            new CleanWebpackPlugin('wwwroot/dist'),
             extractCSS,
-            // Compress extracted CSS.
             new OptimizeCSSPlugin({
                 cssProcessorOptions: {
                     safe: true
                 }
             }),
+            new CopyWebpackPlugin([
+                { from: 'node_modules/geonorge-shared-partials/src/images', to: 'images/'},
+                { from: 'node_modules/geonorge-shared-partials/src/partials', to: 'partials/'},
+                { from: 'node_modules/geonorge-shared-partials/dist', to: 'js/geonorge-shared'},
+                { from: 'node_modules/geonorge-base/src/js', to: 'js/geonorge-base'},
+                { from: 'node_modules/geonorge-base/src/images', to: 'images/'},
+                { from: 'node_modules/geonorge-base/src/fonts', to: 'fonts/'},
+                { from: 'node_modules/bootstrap/dist/fonts', to: 'fonts/'}
+            ]),
             new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.DllPlugin({
                 path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
