de5464af589d2204b4cb189d2cd99988dbd55f15
Dag Olav Dahle <dagolav@arkitektum.no>
2019-10-23 09:16:24 +0200

    Create index for organizationNumber

--- Geonorge.MinSide.Infrastructure/Context/OrganizationContext.cs
+++ Geonorge.MinSide.Infrastructure/Context/OrganizationContext.cs
@@ -11,5 +11,15 @@ namespace Geonorge.MinSide.Infrastructure.Context
         public DbSet<Document> Documents { get; set; }
         public DbSet<Meeting> Meetings { get; set; }
         public virtual DbSet<ToDo> Todo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Document>()
+                .HasIndex(b => b.OrganizationNumber);
+
+            modelBuilder.Entity<Meeting>()
+                .HasIndex(b => b.OrganizationNumber);
+        }
+
     }
 }
---
+++ Geonorge.MinSide.Infrastructure/Migrations/20191023071215_IndexOrganizationNumber.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Geonorge.MinSide.Infrastructure.Migrations
+{
+    public partial class IndexOrganizationNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationNumber",
+                table: "Meetings",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationNumber",
+                table: "Documents",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meetings_OrganizationNumber",
+                table: "Meetings",
+                column: "OrganizationNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_OrganizationNumber",
+                table: "Documents",
+                column: "OrganizationNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Meetings_OrganizationNumber",
+                table: "Meetings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Documents_OrganizationNumber",
+                table: "Documents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationNumber",
+                table: "Meetings",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationNumber",
+                table: "Documents",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
